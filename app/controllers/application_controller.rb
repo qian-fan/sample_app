9ba218d1287f1e30c04d43f4aba1cdba24059b79
qian-fan <iamfanqian@yahoo.com>
2018-05-15 22:30:34 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
-class ApplicationController < ActionController
-  def home
-  end
-
-  def help
-  end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 end
